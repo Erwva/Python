193f816b23bf38375dcb031b5ff1b49aa0f52ddc
Erwva <121205500+Erwva@users.noreply.github.com>
2023-04-17 02:17:52 +0200

    Create zad1.py

---
+++ zad1.py
@@ -0,0 +1,7 @@
+def total_euro(r_sati, p_satu):
+return r_sati*p_satu
+
+r_sati=float(input("radni sati:"))
+po_satu=float(input("satnica:"))
+
+print ("Ukupno: %d eura" % total_euro(r_sati, p_satu))
